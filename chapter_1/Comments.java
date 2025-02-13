05cb6276ce490d0d8fe800b4b9c5c5e62df07c4d
edooarda <edooarda@gmail.com>
2025-02-13 22:25:53 +0100

    add print exercise from chapter 1

---
+++ chapter_1/Comments.java
@@ -0,0 +1,11 @@
+public class Comments {
+    public static void main(String[] args) {
+        System.err.println("Connecting to server…"); //The program attempts to establish a connection to a server.
+        System.err.println("Error: Connection timed out."); // The attempt to connect to the server failed because the connection took too long.
+        System.err.println("Retrying…"); // The program is going to restart.
+
+        // System.err.println() is used to print error messages! 😵😵❌
+        // It is similar to System.out.println() but is used to print error messages.
+
+    }
+}
---
+++ chapter_1/MusicWrap.java
@@ -0,0 +1,21 @@
+/**
+ * Woah? What's this? 🤔 It's a weird looking comment. Another *, eh?
+ * bla ble bla
+ * bbllala
+ *  
+ * I'm a comment, and I want to say hi! 👋
+ */ // this a Javadoc comment !!!!!
+
+public class MusicWrap {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+
+        System.out.println("Heavy is the Crown"); // Linkin Park
+        System.out.println("One more Light"); // Linkin Park
+        System.out.println("Popular Monster"); // Falling in Reverse
+        System.out.println("Sleepwalking"); // Bring me the horizon
+        System.out.println("Kingslayer"); // Bring me the horizon
+
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+    }
+}
