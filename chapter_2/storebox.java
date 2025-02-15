35c4f0d9b1d9f046997ca61bdfc6bbd86a423376
edooarda <edooarda@gmail.com>
2025-02-15 11:00:59 +0100

    add int variable exercise

---
+++ chapter_2/storebox.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int livingRoom = 15; // for variable names Java convention is camelCase
+        int masterBedroom = 45;
+        int bathroom = 10;
+        System.out.println(livingRoom);
+        System.out.println(masterBedroom);
+        System.out.println(bathroom);
+        
+    }
+}
