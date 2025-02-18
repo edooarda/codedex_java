cf78b50287f89fb87d3800f2af77011dd65ac1e9
edooarda <edooarda@gmail.com>
2025-02-18 23:44:37 +0100

    add variables type exercise

---
+++ chapter_2/id_card.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		String name = "Amelie";
+		String country = "France";
+		char gender = 'F';
+		int age = 28;
+		boolean organDonor = true; 
+		System.out.println("ID Card:");
+		System.out.println(name);
+		System.out.println(country);
+		System.out.println(gender);
+		System.out.println(age);
+		System.out.println(organDonor);
+	}
+}
